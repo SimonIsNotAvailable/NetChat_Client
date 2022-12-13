64bb1f9e1fd4885448a93b2786f90eacdfc70746
SimonIsNotAvailable <polushkinsn@gmail.com>
2022-12-13 13:10:27 +0500

    Client classes done

--- NetChat_Client/src/main/java/Client/MessageListener.java
+++ NetChat_Client/src/main/java/Client/MessageListener.java
@@ -4,19 +4,42 @@ import Logger.Logger;
 import Logger.LogType;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class MessageListener implements Runnable{
 
     private Logger log = Logger.getInstance();
     private BufferedReader in;
-    private PrintWriter out;
+
     public MessageListener (BufferedReader in){
 
     }
 
     @Override
     public void run() {
+        while (readMessage());
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private boolean readMessage() {
+        String msg;
+        try {
+            msg = in.readLine();
+            if(msg != null) {
+                System.out.println(msg);
+                log.log(msg, LogType.MESSAGE, true);
+            }
+        } catch (IOException e) {
+            String troubles = "| Troubles with connection |";
+            System.out.println(troubles);
+            log.log(troubles, LogType.ERROR, false);
+            return false;
+        }
+        return true;
     }
 }
--- NetChat_Client/src/main/java/Client/MessageSender.java
+++ NetChat_Client/src/main/java/Client/MessageSender.java
@@ -1,15 +1,39 @@
 package Client;
 
 import java.io.PrintWriter;
+import java.util.Scanner;
+
 import Logger.Logger;
 import Logger.LogType;
 
 public class MessageSender implements Runnable{
+    private PrintWriter out;
+    private Logger log = Logger.getInstance();
+    private Scanner scanner = new Scanner(System.in);
+
     public MessageSender(PrintWriter out) {
+    this.out = out;
     }
 
     @Override
     public void run() {
-
+        String msg;
+        while (true) {
+            msg = scanner.nextLine();
+            if (!sendMessage(msg)) break;
+        }
+        System.exit(0);
+    }
+    public boolean sendMessage(String message){
+        out.println(message);
+        if (message.equalsIgnoreCase("/exit")){
+            String byeMessage = "| You left the chat |";
+            log.log(byeMessage, LogType.INFO, false);
+            return false;
+        } else {
+            String yourMessage = "You: " + message;
+            log.log(yourMessage, LogType.MESSAGE, false);
+        }
+        return true;
     }
 }
